2ffbe63a32377e9dee47cc047c12431d5acd6b00
Rei Yasuda <74781346+tontonbutadon@users.noreply.github.com>
2023-12-02 08:15:14 +0000

    Update 7 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/Header";
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
+  const [tabContent, setTabContent] = useState("");
+
+  const handleSelect = (selectedBtn) => {
+    //selectedbtn string　⇒ "components", "jsx", "props", "state"
+    setTabContent(selectedBtn);
+    console.log(tabContent);
+  };
+
+  let contentTab = <p>Please selct a topic</p>;
+  if (tabContent) {
+    contentTab = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[tabContent].title}</h3>
+        <p>{EXAMPLES[tabContent].description}</p>
+        <pre>{EXAMPLES[tabContent].code}</pre>
+        <code></code>
+      </div>
+    );
+  }
+
+  console.log("App Component Executing");
   return (
     <div>
       <Header />
@@ -10,8 +35,11 @@ function App() {
         <h2>CORE CONCEPTS</h2>
         <section id="core-concepts">
           <ul>
+            {CORE_CONCEPTS.map((conceptItem) => {
+              <CoreConcept key={conceptItem} {...conceptItem} />;
+            })}
+            {/*
             <CoreConcept
-              title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
@@ -30,7 +58,38 @@ function App() {
               description={CORE_CONCEPTS[3].description}
               image={CORE_CONCEPTS[3].image}
             />
+            */}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={tabContent === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={tabContent === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={tabContent === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={tabContent === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              States
+            </TabButton>
+          </menu>
+          {contentTab}
         </section>
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+export default function TabButton({ children, onSelect, isSelected }) {
+  console.log("TabButton Component Executing");
+  return (
+    <li>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
+    </li>
+  );
+}
